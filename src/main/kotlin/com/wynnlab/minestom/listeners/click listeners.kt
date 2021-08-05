e4074ca061793bf4d45fb64f307adc253664a02b
FauxKiwi <sfn14ok04@gmail.com>
2021-08-05 20:47:32 +0200

    Added click sequences

--- src/main/kotlin/com/wynnlab/minestom/listeners/click listeners.kt
+++ src/main/kotlin/com/wynnlab/minestom/listeners/click listeners.kt
@@ -1,10 +1,17 @@
 package com.wynnlab.minestom.listeners
 
+import com.wynnlab.minestom.gui.MenuGui
+import com.wynnlab.minestom.tasks.RefreshDelayTask
 import com.wynnlab.minestom.util.listen
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
 import net.minestom.server.entity.Player
 import net.minestom.server.event.EventFilter
 import net.minestom.server.event.EventNode
 import net.minestom.server.event.player.*
+import net.minestom.server.tag.Tag
+import net.minestom.server.utils.time.TimeUnit
 
 val playerSpellClickListenersNode = EventNode.type("player-spell-click-listeners", EventFilter.PLAYER) { e, player ->
     e is PlayerBlockInteractEvent && e.hand == Player.Hand.MAIN
@@ -23,11 +30,90 @@ private fun onPlayerUseItem(e: PlayerUseItemEvent) {
 }
 
 private fun onPlayerRightClick(player: Player) {
-    player.sendMessage("RC")
+    when (player.heldSlot.toInt()) {
+        6 -> MenuGui().show(player)
+        7, 8 -> {}
+        else -> {
+            noAb(player)
+            val seq = addToClickSequence(player, true)
+            showClickSequence(player, seq)
+            val spell = when (seq) {
+                16.toByte() /* rlr */ -> 1
+                13.toByte() /* rrr */ -> 2
+                else -> -1
+            }
+            if (spell > 0) castSpellAndResetClickSequence(player, spell)
+        }
+    }
 }
 
 private fun onPlayerLeftClick(player: Player) {
-    player.sendMessage("LC")
+    if (player.heldSlot > 5) return
+    val seq = addToClickSequence(player, false)
+    if (seq == 2.toByte()) // 1x left-click
+        castSpellAndResetClickSequence(player, 0)
+    else {
+        noAb(player)
+        showClickSequence(player, seq)
+        val spell = when (seq) {
+            25.toByte() /* rll */ -> 3
+            22.toByte() /* rrl */ -> 4
+            else -> -1
+        }
+        if (spell > 0) castSpellAndResetClickSequence(player, spell)
+    }
+}
+
+private fun addToClickSequence(player: Player, rightClick: Boolean): Byte {
+    val count = if (rightClick) 1 else 2
+    val prev = player.getTag(clickSequenceTag)!!.toInt()
+    val new = (prev + (if (prev == 0) count else if (prev >= 3) count * 3 * 3 else count * 3)).toByte()
+    player.setTag(clickSequenceTag, new)
+    return new
+}
+
+private fun showClickSequence(player: Player, sequence: Byte) {
+    val c1 = sequence % 3
+    val c2 = (sequence / 3) % 3
+    val c3 = sequence / 9
+    val hyphen = Component.text("-", NamedTextColor.GRAY)
+    player.sendActionBar(Component.text()
+        .append(clickSequenceAbChar(c1))
+        .append(hyphen)
+        .append(clickSequenceAbChar(c2))
+        .append(hyphen)
+        .append(clickSequenceAbChar(c3))
+        .build())
+}
+
+private fun clickSequenceAbChar(c: Int) = when (c) {
+    1 -> Component.text("R", NamedTextColor.GREEN, TextDecoration.UNDERLINED)
+    2 -> Component.text("L", NamedTextColor.GREEN, TextDecoration.UNDERLINED)
+    else -> Component.text("?", NamedTextColor.WHITE)
+}
+
+// stored as base 3 value: r = 1, l = 2
+// r-?-? = 1
+// r-r-? = 4; r-l-? = 7
+private val clickSequenceTag = Tag.Byte("click-sequence").defaultValue(0)
+
+private fun noAb(player: Player) {
+    player.setTag(noAbTag, 1)
+    RefreshDelayTask(player, "no-ab") {
+        player.removeTag(noAbTag)
+        resetClickSequence(player)
+    }.schedule(1, TimeUnit.SECOND)
+}
+
+private val noAbTag = Tag.Byte("no-ab")
+
+private fun castSpellAndResetClickSequence(player: Player, spell: Int) {
+    player.sendMessage("spell $spell")
+    resetClickSequence(player)
+}
+
+private fun resetClickSequence(player: Player) {
+    player.setTag(clickSequenceTag, 0)
 }
 
 fun initClickListeners() {
---
+++ src/main/kotlin/com/wynnlab/minestom/tasks/RefreshDelayPlayerTask.kt
@@ -0,0 +1,43 @@
+package com.wynnlab.minestom.tasks
+
+import net.minestom.server.MinecraftServer
+import net.minestom.server.tag.Tag
+import net.minestom.server.tag.TagReadable
+import net.minestom.server.tag.TagWritable
+import net.minestom.server.utils.time.TimeUnit
+import java.time.temporal.TemporalUnit
+
+abstract class RefreshDelayTask<DataContainer>(
+    private val data: DataContainer,
+    id: String
+) : Runnable where DataContainer : TagReadable, DataContainer : TagWritable {
+    private val tag = Tag.Integer("RDT-$id").defaultValue(0)
+
+    final override fun run() {
+        val delay = data.getTag(tag)!!
+        if (delay > 0)
+            data.setTag(tag, delay - 1)
+        if (delay <= 1) {
+            task()
+            data.removeTag(tag)
+        }
+    }
+
+    abstract fun task()
+
+    fun schedule(delay: Long, unit: TemporalUnit) {
+        MinecraftServer.getSchedulerManager().buildTask(this).delay(delay, unit).schedule()
+        data.setTag(tag, data.getTag(tag)!! + 1)
+    }
+}
+
+inline fun <DataContainer> RefreshDelayTask(
+    data: DataContainer,
+    id: String,
+    crossinline task: () -> Unit
+) where DataContainer : TagReadable, DataContainer : TagWritable =
+    object : RefreshDelayTask<DataContainer>(data, id) {
+        override fun task() {
+            task()
+        }
+    }
