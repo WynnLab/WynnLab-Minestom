3fc6aa3b94b3ae74fe8217993e40b17f71d05406
FauxKiwi <sfn14ok04@gmail.com>
2021-08-09 15:22:24 +0200

    Added type option to dummies

--- src/main/kotlin/com/wynnlab/minestom/commands/DummyCommand.kt
+++ src/main/kotlin/com/wynnlab/minestom/commands/DummyCommand.kt
@@ -11,21 +11,20 @@ object DummyCommand : Command("dummy") {
         setCondition { sender, _ -> sender.isPlayer }
 
         val healthArg = ArgumentType.Integer("health").min(1).setDefaultValue(100_000)
+        val typeArg = ArgumentType.EntityType("type").setDefaultValue(EntityType.ARMOR_STAND)
 
         addSyntax({ sender, ctx ->
             val player = sender.asPlayer()
             val instance = player.instance ?: return@addSyntax
             val maxHealth = ctx[healthArg].toFloat()
 
-            val dummy = CustomEntity.Default(EntityType.VINDICATOR)
+            val dummy = CustomEntity.Default(ctx[typeArg])
             dummy.maxHealth = maxHealth
             dummy.health = maxHealth
             dummy.customName = Component.text("Dummy")
             dummy.isCustomNameVisible = true
 
-
-
             dummy.setInstance(instance, player.position)
-        }, healthArg)
+        }, healthArg, typeArg)
     }
 }
